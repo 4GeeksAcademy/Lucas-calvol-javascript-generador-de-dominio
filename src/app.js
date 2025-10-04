a8488ad1bc003af233767c14d350ca049d811f17
Lucas Matias Calvo <lucascalvo2903@gmail.com>
2025-10-04 03:37:08 +0000

    correcion de funciones

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
+window.onload = function () {
   let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
-  let domain = ['.com','.es','.net'];
+  let domain = ['.com', '.es', '.net'];
 
-  for (let p = 0; p < pronoun.length; p++) {
-    for (let a = 0; a < adj.length; a++) {
-      for (let n = 0; n < noun.length; n++) {
-        for (let d = 0; d <domain.length; d++){
-        console.log(pronoun[p] + adj[a] + noun[n] + domain[d]);
+  let domains = generateDomains(pronoun, adj, noun, domain);
+  console.log(domains);
+};
+
+function generateDomains(pronoun, adj, noun, domain) {
+  let results = [];
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let d of domain) {
+          results.push(`${p}${a}${n}${d}`);
+        }
       }
     }
   }
-  }
-};
-
+  return results;
+}
